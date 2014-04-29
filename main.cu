153b46d9b494aa1714e025d2d2429d7867f603e4
zequiel <aramel@epsi.fr>
2014-04-29 15:08:47 +0200

    Modification du blur, Animation du blur en continue.

--- main.cu
+++ main.cu
@@ -1,24 +1,29 @@
 #include "image.h"
-#include "common/cpu_bitmap.h"
+#include "common/cpu_anim.h"
 #include <iostream>
+#include <time.h>
+
+bool add = true;
+
+void cleanup( DataBlock *d ) {
+	cudaFree( d->dev_bitmap );
+	cudaFree( d->dev_output);
+}
 
 __device__ int getPixelIndex(int x, int y, int width) {
 	int offset = y * width + x;
 	return offset * 4;
 }
-__global__ void kernel(unsigned char *pixels, int width, int height,
+__global__ void kernel(unsigned char *pixels, unsigned char *output, int width, int height,
 		int radius) {
 	int x, y;
 	x = blockIdx.x * blockDim.x + threadIdx.x;
 	y = blockIdx.y * blockDim.y + threadIdx.y;
 	if (x < width && y < height) {
-		unsigned int averageColor[3];
+		unsigned int averageColor[3] = {0, 0, 0};
 		int pixelOffset = getPixelIndex(x, y, width);
-		averageColor[0] = pixels[pixelOffset];
-		averageColor[1] = pixels[pixelOffset + 1];
-		averageColor[2] = pixels[pixelOffset + 2];
 
-		int count = 1;
+		int count = 0;
 
 		int currentOffset;
 		for (int d_x = -radius; d_x <= radius; ++d_x) {
@@ -40,37 +45,48 @@ __global__ void kernel(unsigned char *pixels, int width, int height,
 		averageColor[0] /= count;
 		averageColor[1] /= count;
 		averageColor[2] /= count;
-		__syncthreads();
-		pixels[pixelOffset + 0] = averageColor[0];
-		pixels[pixelOffset + 1] = averageColor[1];
-		pixels[pixelOffset + 2] = averageColor[2];
-	} else {
-		__syncthreads();
+
+		output[pixelOffset + 0] = averageColor[0];
+		output[pixelOffset + 1] = averageColor[1];
+		output[pixelOffset + 2] = averageColor[2];
+		output[pixelOffset + 3] = 255;
 	}
 }
 
-int main() {
-	Image image;
-	image.loadFromFile("madame.jpg");
-	CPUBitmap bitmap(image.width(), image.height());
-	unsigned char *cudaPixels;
+void generateFrame(DataBlock *d, int ticks)
+{
+	if ((ticks % 50) == 0)
+		add = !add;
 
-	std::size_t pixelsSize = sizeof(unsigned char) * image.width()
-			* image.height() * 4;
+	if ((ticks % 5) == 0)
+		d->radius = (add)?d->radius+1:d->radius-1;
 
-	cudaMalloc(&cudaPixels, pixelsSize);
-	cudaMemcpy(cudaPixels, image.pixels(), pixelsSize, cudaMemcpyHostToDevice);
-	std::cout << image.width() / 16 << ":"<< image.height() / 16 << std::endl;
+	std::size_t pixelsSize = sizeof(unsigned char) * d->image.width()
+				* d->image.height() * 4;
+	cudaMemcpy(d->dev_bitmap, d->image.pixels(), pixelsSize, cudaMemcpyHostToDevice);
 
-	kernel<<<dim3((image.width() / 16) + 1, (image.height() / 16) + 1),
-			dim3(17, 17)>>>(cudaPixels,
-			image.width(), image.height(), 0);
+	kernel<<<dim3((d->image.width() / 16) + 1, (d->image.height() / 16) + 1),
+			dim3(17, 17)>>>(d->dev_bitmap, d->dev_output,
+					d->image.width(), d->image.height(), d->radius);
 
-	cudaMemcpy(bitmap.get_ptr(), cudaPixels, pixelsSize,
+	cudaMemcpy(d->bitmap->get_ptr(), d->dev_output, pixelsSize,
 			cudaMemcpyDeviceToHost);
-	cudaFree(cudaPixels);
+}
 
-	bitmap.display_and_exit();
+int main() {
+	DataBlock data;
+	data.radius = 1;
+	data.image.loadFromFile("madame.jpg");
+	CPUAnimBitmap bitmap(data.image.width(), data.image.height(), &data);
+	data.bitmap = &bitmap;
+
+	std::size_t pixelsSize = sizeof(unsigned char) * data.image.width()
+			* data.image.height() * 4;
+
+	cudaMalloc(&data.dev_output, pixelsSize);
+	cudaMalloc(&data.dev_bitmap, pixelsSize);
+
+	bitmap.anim_and_exit( (void (*)(void*,int))generateFrame, (void (*)(void*))cleanup );
 
 	return 0;
 }
